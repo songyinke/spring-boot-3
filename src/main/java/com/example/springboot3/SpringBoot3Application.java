0fbc0f7739183446deed00571d6cc0d9592cd01d
songyinke <songyinke@icloud.com>
2024-12-18 15:06:07 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot3/SpringBoot3Application.java
@@ -0,0 +1,14 @@
+package com.example.springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(SpringBoot3Application.class, args);
+    }
+
+}
